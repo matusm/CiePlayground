bcd8f2babcc40491e08ab523749736f86ed00dde
Michael Matus <michael@matus.at>
2022-08-29 19:51:57 +0200

    Intermediate

--- CiePlayground/Program.cs
+++ CiePlayground/Program.cs
@@ -10,18 +10,19 @@ namespace CiePlayground
     {
         public static void Main(string[] args)
         {
-            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
             SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_8421_Kalibrierung_FEL1000_2022_intpl1.csv");
             Console.WriteLine($"CCT = {lamp.ColorTemperature.Cct:F3} K  ({lamp.ColorTemperature.ChomaticityDifference:F9}   {lamp.ColorTemperature.Status})");
             Console.WriteLine();
 
+            // basic MC simulation
             StatisticPod sp = new StatisticPod("CCT / K");
             SpectralQuantity lampR;
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 50; i++)
             {
                 lampR = lamp.Randomize(0, 0.05);
-                Console.WriteLine($"CCT = {lampR.ColorTemperature.Cct:F3} K  ({lampR.ColorTemperature.ChomaticityDifference:F9}   {lampR.ColorTemperature.Status})");
+                Console.WriteLine($"{i,5}   {lampR.ColorTemperature.Cct:F1} K  ({lampR.ColorTemperature.ChomaticityDifference:F9}   {lampR.ColorTemperature.Status})");
                 sp.Update(lampR.ColorTemperature.Cct);
             }
             Console.WriteLine();
