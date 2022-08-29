5e49c7f1f64274b0bac4024dd2c1d0dfc21a86f8
Michael Matus <michael@matus.at>
2022-08-29 22:09:01 +0200

    Streamline source

--- CiePlayground/Program.cs
+++ CiePlayground/Program.cs
@@ -12,21 +12,21 @@ namespace CiePlayground
         {
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-            SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_7-1108_Kalibrierung_FEL1000_2022_intpl1.csv");
-            Console.WriteLine($"CCT = {lamp.ColorTemperature.Cct:F3} K  ({lamp.ColorTemperature.ChomaticityDifference:F9}   {lamp.ColorTemperature.Status})");
-            Console.WriteLine();
+            SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_8421_Kalibrierung_FEL1000_2022_intpl1.csv");
 
             // basic MC simulation
             StatisticPod sp = new StatisticPod("CCT / K");
             SpectralQuantity lampR;
-            for (int i = 0; i < 50; i++)
+            for (int i = 0; i < 500; i++)
             {
-                lampR = lamp.Randomize(0, 0.005);
-                Console.WriteLine($"CCT = {lampR.ColorTemperature.Cct:F3} K  ({lampR.ColorTemperature.ChomaticityDifference:F9}   {lampR.ColorTemperature.Status})");
+                lampR = lamp.Randomize(0, 0.006);
+                Console.WriteLine($"{i,5}   {lampR.ColorTemperature.Cct:F1} K   ({lampR.ColorTemperature.ChomaticityDifference:F9} -> {lampR.ColorTemperature.Status})");
                 sp.Update(lampR.ColorTemperature.Cct);
             }
+
             Console.WriteLine();
-            Console.WriteLine(sp);
+            Console.WriteLine($"CCT of original spectrum: {lamp.ColorTemperature.Cct:F1} K ({lamp.ColorTemperature.ChomaticityDifference:F9} -> {lamp.ColorTemperature.Status})");
+            Console.WriteLine($"CCT of MC spectra:        {sp.AverageValue:F1} ({sp.StandardDeviation:F1}) K");
 
         }
 
