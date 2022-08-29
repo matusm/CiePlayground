0d586ddf28bd9c391010ebbc9eddabdfcb9316ee
Michael Matus <michael@matus.at>
2022-08-29 19:53:42 +0200

    Resolved

--- CiePlayground/Program.cs
+++ CiePlayground/Program.cs
@@ -12,7 +12,7 @@ namespace CiePlayground
         {
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-            SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_8421_Kalibrierung_FEL1000_2022_intpl1.csv");
+            SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_7-1108_Kalibrierung_FEL1000_2022_intpl1.csv");
             Console.WriteLine($"CCT = {lamp.ColorTemperature.Cct:F3} K  ({lamp.ColorTemperature.ChomaticityDifference:F9}   {lamp.ColorTemperature.Status})");
             Console.WriteLine();
 
@@ -21,8 +21,11 @@ namespace CiePlayground
             SpectralQuantity lampR;
             for (int i = 0; i < 50; i++)
             {
-                lampR = lamp.Randomize(0, 0.05);
-                Console.WriteLine($"{i,5}   {lampR.ColorTemperature.Cct:F1} K  ({lampR.ColorTemperature.ChomaticityDifference:F9}   {lampR.ColorTemperature.Status})");
+<<<<<<< HEAD
+=======
+                lampR = lamp.Randomize(0, 0.005);
+                Console.WriteLine($"CCT = {lampR.ColorTemperature.Cct:F3} K  ({lampR.ColorTemperature.ChomaticityDifference:F9}   {lampR.ColorTemperature.Status})");
+>>>>>>> 7b873f2963754d79ba96cbae6ea00be84cce30d6
                 sp.Update(lampR.ColorTemperature.Cct);
             }
             Console.WriteLine();
