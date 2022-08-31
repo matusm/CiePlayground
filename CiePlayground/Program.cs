5fbe8a4c5d9f912f182d7c7c15fff0e48962fcd9
Michael Matus <michael@matus.at>
2022-08-31 22:17:33 +0200

    Nice MC calculation

--- CiePlayground/Program.cs
+++ CiePlayground/Program.cs
@@ -14,22 +14,32 @@ namespace CiePlayground
 
             SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_7-1108_Kalibrierung_FEL1000_2022_noU.csv");
 
-            Console.WriteLine($"CCT of original spectrum: {lamp.CCT:F1} K ({lamp.ColorTemperature.ChomaticityDifference:F9} -> {lamp.ColorTemperature.Status})");
-            Console.WriteLine($"Distribution temperature of original spectrum: {lamp.TD:F1} K");
+            Console.WriteLine($"Name:                     {lamp.Name}");
+            Console.WriteLine($"Number of values:         {lamp.NumberOfValues}");
+            Console.WriteLine($"Minimum wavelength:       {lamp.MinWavelength} nm");
+            Console.WriteLine($"Maximum wavelength:       {lamp.MaxWavelength} nm");
+            Console.WriteLine($"Color temperature:        {lamp.CCT} K");
+            Console.WriteLine($"Distribution temperature: {lamp.TD} K");
+            Console.WriteLine();
 
             // basic MC simulation
-        //    StatisticPod sp = new StatisticPod("CCT / K");
-        //    SpectralQuantity lampR;
-        //    for (int i = 0; i < 5; i++)
-        //    {
-        //        lampR = lamp.Randomize(0, 0.006);
-        //        Console.WriteLine($"{i,5}   {lampR.ColorTemperature.Cct:F1} K   ({lampR.ColorTemperature.ChomaticityDifference:F9} -> {lampR.ColorTemperature.Status})");
-        //        sp.Update(lampR.ColorTemperature.Cct);
-        //    }
+            CovariancePod sp = new CovariancePod();
+            SpectralQuantity lampR;
+            for (int i = 0; i <= 100; i++)
+            {
+                lampR = lamp.Randomize(0, 0.006);
+                if(i%10==0)
+                    Console.Write("|");
+                else
+                    Console.Write("-");
+                sp.Update(lampR.CCT, lampR.TD);
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"CCT of MC spectra: {sp.AverageValueOfX:F1} ({sp.StandardDeviationOfX:F1}) K");
+            Console.WriteLine($"T_D of MC spectra: {sp.AverageValueOfY:F1} ({sp.StandardDeviationOfY:F1}) K");
+            Console.WriteLine($"correlation coefficient: {sp.CorrelationCoefficient:F4}");
 
-        //    Console.WriteLine();
-        //    Console.WriteLine($"CCT of original spectrum: {lamp.ColorTemperature.Cct:F1} K ({lamp.ColorTemperature.ChomaticityDifference:F9} -> {lamp.ColorTemperature.Status})");
-        //    Console.WriteLine($"CCT of MC spectra:        {sp.AverageValue:F1} ({sp.StandardDeviation:F1}) K");
 
         }
 
