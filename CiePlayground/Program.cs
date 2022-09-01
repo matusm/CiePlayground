6e34f51f5ff0d29d2d244e675318a76e61976c90
Michael Matus <michael@matus.at>
2022-09-01 15:14:49 +0200

    Use new library

--- CiePlayground/Program.cs
+++ CiePlayground/Program.cs
@@ -12,20 +12,20 @@ namespace CiePlayground
         {
             CultureInfo.CurrentCulture = CultureInfo.InvariantCulture;
 
-            SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_7-1108_Kalibrierung_FEL1000_2022_noU.csv");
+            SpectralQuantity lamp = SpectralQuantity.LoadFromCsv("SN_8421_Kalibrierung_FEL1000_2022_noU.csv");
 
             Console.WriteLine($"Name:                     {lamp.Name}");
             Console.WriteLine($"Number of values:         {lamp.NumberOfValues}");
             Console.WriteLine($"Minimum wavelength:       {lamp.MinWavelength} nm");
             Console.WriteLine($"Maximum wavelength:       {lamp.MaxWavelength} nm");
-            Console.WriteLine($"Color temperature:        {lamp.CCT} K");
-            Console.WriteLine($"Distribution temperature: {lamp.TD} K");
+            Console.WriteLine($"Color temperature:        {lamp.ColorTemperature.CCT} K [{lamp.ColorTemperature.ChomaticityDifference:F9}]");
+            Console.WriteLine($"Distribution temperature: {lamp.DistributionTemperature.TD} K [{lamp.DistributionTemperature.RelativeDeviation*100:F1} %]");
             Console.WriteLine();
 
             // basic MC simulation
             CovariancePod sp = new CovariancePod();
             SpectralQuantity lampR;
-            for (int i = 0; i <= 100; i++)
+            for (int i = 0; i <= 10; i++)
             {
                 lampR = lamp.Randomize(0, 0.006);
                 if(i%10==0)
